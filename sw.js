e87358089b4101d99fd67cd7921956a1b555d3b5
mobilefirststudio <mobilefirststudio@gmail.com>
2026-09-20 19:40:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mfs-pin-queue-v1";
+const CACHE_NAME = "mfs-pin-queue-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(event) {
